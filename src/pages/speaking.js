7c1a50afdbb456ae2dc3b9248662f8435b5a218b
Dr Amanda Kavner <78941808+drkavner@users.noreply.github.com>
2023-08-09 20:31:10 -0400

    fix

--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -1,4 +1,5 @@
 import React from "react"
+import https://github.com/gatsbyjs/gatsby-starter-wordpress-homepage from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
